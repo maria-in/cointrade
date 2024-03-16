c7a881bf36f2446c2cc77401afb4591ead541147
Mia <mariamasha12345@gmail.com>
2024-03-16 18:07:07 +0300

    feat(coingeckoapi): dependencies serialization lib

--- coingeckoapi/build.gradle.kts
+++ coingeckoapi/build.gradle.kts
@@ -13,4 +13,5 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlin.serialization.json)
+    implementation(libs.retrofit.serialization.converter)
 }
