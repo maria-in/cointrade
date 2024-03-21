07ec7d33d84f100c418471d22b9a26fd9acd13e1
Mia <mariamasha12345@gmail.com>
2024-03-21 17:12:05 +0300

    feat(database): create

--- database/src/main/java/com/kalinchuk/coin/database/CoinDatabase.kt
+++ database/src/main/java/com/kalinchuk/coin/database/CoinDatabase.kt
@@ -1,5 +1,30 @@
 package com.kalinchuk.coin.database
 
-class CoinDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.kalinchuk.coin.database.dao.CoinDao
+import com.kalinchuk.coin.database.models.CoinEntity
+import com.kalinchuk.coin.database.utils.Converters
 
+class CoinDatabase internal constructor(private val database: RoomCoinDatabase) {
+    val coinDao: CoinDao
+        get() = database.coinDao()
+}
+
+@Database(entities = [CoinEntity::class], version = 1)
+@TypeConverters(Converters::class)
+internal abstract class RoomCoinDatabase : RoomDatabase() {
+    abstract fun coinDao(): CoinDao
+}
+
+fun CoinDatabase(applicationContext: Context): CoinDatabase {
+    val coinRoomDatabase = Room.databaseBuilder(
+        checkNotNull(applicationContext.applicationContext),
+        RoomCoinDatabase::class.java,
+        "coins-db"
+    ).build()
+    return CoinDatabase(coinRoomDatabase)
 }
---
+++ database/src/main/java/com/kalinchuk/coin/database/dao/CoinDao.kt
@@ -0,0 +1,24 @@
+package com.kalinchuk.coin.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.kalinchuk.coin.database.models.CoinEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CoinDao {
+
+    @Query("SELECT * FROM coin_list")
+    fun getCoinsList(): Flow<List<CoinEntity>>
+
+    @Insert
+    suspend fun insertAllCoins(coins: List<CoinEntity>?)
+
+    @Delete
+    suspend fun removeAllCoins(coins: List<CoinEntity>)
+
+    @Query("DELETE FROM coin_list")
+    suspend fun clearAllCoins()
+}
